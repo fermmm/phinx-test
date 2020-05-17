42fea58392511c2469e9189d6b89ba77d0ba4314
Fernando Medina <fermmm@gmail.com>
2020-05-17 04:51:45 -0300

    Uploaded snapshot

--- src/components/pages/MainPage/Tests/MainPage.test.tsx
+++ src/components/pages/MainPage/Tests/MainPage.test.tsx
@@ -1,6 +1,9 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { MainPage } from "../MainPage";
+import MainPage from "../MainPage";
+import * as hooks from "../../../../tools/hooks";
+
+jest.spyOn(hooks, 'useUrlQuery').mockImplementation(() => ({}));
 
 describe("<MainPage />", () => {
    test("Renders correctly", () => {
