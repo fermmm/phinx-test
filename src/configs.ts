f7a7856b6a6496c7c16fb559e65bc6b99a6c1e3b
Fernando Medina <fermmm@gmail.com>
2020-05-17 06:12:06 -0300

    Changed server url to https

--- src/configs.ts
+++ src/configs.ts
@@ -2,5 +2,5 @@
  * I know this should be on the .env file and not uploaded to github but this is just a demo
  * and I want to simplify the deployment.
  */
-export const SERVER_URL = "http://gateway.marvel.com/v1/public/";
+export const SERVER_URL = "https://gateway.marvel.com/v1/public/";
 export const PUBLIC_API_KEY = "cb67da1cc7061bcd0cc318c4314c3e45";
