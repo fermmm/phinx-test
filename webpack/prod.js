f2de8dadb9127a1e9617f7e7ced80237d4aa6ad3
Fernando Medina <fermmm@gmail.com>
2020-05-17 05:02:08 -0300

    Fixed not building issue

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,11 +1,15 @@
-const merge = require('webpack-merge');
-const path = require('path');
-const baseConfig = require('./base.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require("webpack-merge");
+const path = require("path");
+const baseConfig = require("./base.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const distDir = path.resolve(__dirname, '../dist/');
+const distDir = path.resolve(__dirname, "../dist/");
 
 module.exports = merge(baseConfig, {
-   mode: 'production',
-   plugins: [new CleanWebpackPlugin([distDir], { root: process.cwd() })],
+   mode: "production",
+   plugins: [
+      new CleanWebpackPlugin({
+         cleanAfterEveryBuildPatterns: ["dist"],
+      }),
+   ],
 });
